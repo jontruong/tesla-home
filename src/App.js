d9de987385ddbcbdf8f9a193d6d88f4620488c7f
Jonathan Truong <jjtruong90@gmail.com>
2021-03-02 19:37:08 -0800

    Header + assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Header />
+      {/* Items => snapping */}
+      {/* Items => snapping */}
+      {/* Items => snapping */}
+      {/* Items => snapping */}
     </div>
   );
 }
